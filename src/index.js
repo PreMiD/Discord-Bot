a856829ff649894ac5157d39b84951effc674927
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-01-27 03:45:25 +0100

    Just disabling some unused things...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,37 @@ require('dotenv').load();
 //* Require stuff
 var fs = require('fs')
 const Discord = require('discord.js'),
-client = new Discord.Client();
+client = new Discord.Client({
+  disabledEvents: [
+    "GUILD_DELETE",
+    "GUILD_MEMBER_REMOVE",
+    "GUILD_MEMBER_UPDATE",
+    "GUILD_ROLE_CREATE",
+    "GUILD_ROLE_DELETE",
+    "GUILD_ROLE_UPDATE",
+    "GUILD_BAN_ADD",
+    "GUILD_BAN_REMOVE",
+    "CHANNEL_CREATE",
+    "CHANNEL_DELETE",
+    "CHANNEL_UPDATE",
+    "CHANNEL_PINS_UPDATE",
+    "MESSAGE_DELETE",
+    "MESSAGE_UPDATE",
+    "MESSAGE_DELETE_BULK",
+    "MESSAGE_REACTION_ADD",
+    "MESSAGE_REACTION_REMOVE",
+    "MESSAGE_REACTION_REMOVE_ALL",
+    "USER_UPDATE",
+    "USER_NOTE_UPDATE",
+    "USER_SETTINGS_UPDATE",
+    "PRESENCE_UPDATE",
+    "VOICE_STATE_UPDATE",
+    "TYPING_START",
+    "VOICE_SERVER_UPDATE",
+    "RELATIONSHIP_ADD",
+    "RELATIONSHIP_REMOVE"
+  ]
+});
 //* Load events
 require('./util/eventLoader')(client);
 
