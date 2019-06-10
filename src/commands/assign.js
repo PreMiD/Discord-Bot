1a87992d4642318f934b6863c16441197f38f7e4
CRUGG <ich@crugg.de>
2019-06-10 09:21:32 +0200

    Update assign.js

--- src/commands/assign.js
+++ src/commands/assign.js
@@ -3,24 +3,17 @@ var assignRoles = require('../assignRoles.json');
 exports.run = async (client, message, params) => {
 	if (params.length < 1 || !assignRoles.find((r) => r.name.toLowerCase() == params.join(' ').toLowerCase())) {
 		message.delete();
-		message
-			.reply(
-				`These roles are assignable: ${assignRoles
-					.map((r) => {
-						return `**${r.name}**`;
-					})
-					.join(', ')}`
-			)
-			.then((msg) => setTimeout(() => msg.delete(), 15 * 1000));
+		message.reply("These roles are assignable:" + assignRoles.map((r) => {return "**" + r.name + "**";}).join(', ')})
+		.then((msg) => setTimeout(() => msg.delete(), 15 * 1000));
 		return;
 	}
 
 	var assignRole = message.guild.roles.find((r) => r.name.toLowerCase() == params.join(' ').toLowerCase());
 	if (message.member.roles.has(assignRole.id)) {
-		message.react('❌').then(() => setTimeout(() => message.delete(), 15 * 1000));
-		message.reply('You already have this role.').then((msg) => setTimeout(() => msg.delete(), 15 * 1000));
+		message.react("❌").then(() => setTimeout(() => message.delete(), 15 * 1000));
+		message.reply("You already have this role.").then((msg) => setTimeout(() => msg.delete(), 15 * 1000));
 	} else {
-		message.react('✅').then(() => setTimeout(() => message.delete(), 15 * 1000));
+		message.react("✅").then(() => setTimeout(() => message.delete(), 15 * 1000));
 		message.member.addRole(assignRole);
 	}
 };
