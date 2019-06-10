f8f3169930b46e53bb80e84eed19ad4ea634c838
CRUGG <ich@crugg.de>
2019-06-10 09:26:09 +0200

    Update clearwarnings.js

--- src/commands/clearwarnings.js
+++ src/commands/clearwarnings.js
@@ -4,31 +4,23 @@ var { query } = require('../database/functions');
 exports.run = async (client, message, params) => {
 	await message.delete();
 	if (params.length < 1) {
-		message
-			.reply(`Usage: \`\`${prefix}clearwarn <user>\`\``)
-			.then((msg) => setTimeout(() => msg.delete(), 10 * 1000));
+		message.reply(`Usage: \`\`${prefix}clearwarn <user>\`\``).then((msg) => setTimeout(() => msg.delete(), 10 * 1000));
 	} else {
 		var user = message.mentions.users.first();
-		if (!user)
+		if (!user) {
 			user = await message.guild.members.find((m) => m.displayName == params.slice(0, params.length).join(' '));
+		}
 		if (user) {
 			var warns = (await query('SELECT COUNT(*) AS warns FROM warns WHERE user_id = ?', user.id)).rows[0].warns;
 			if (!user.bot) {
 				if (warns == 0) {
-					message.channel.send(
-						`**${message.guild.members.get(user.id).displayName}** does not have any warnings!`
-					);
+					message.channel.send("**" + message.guild.members.get(user.id).displayName + "** does not have any warnings!");
 				} else {
 					await query('DELETE FROM warns WHERE user_id = ?', user.id);
-					message.channel.send(
-						`<@${user.id}>, **${message.guild.members.get(user.id)
-							.displayName}** cleared all your warnings! You must have been lucky! :four_leaf_clover: `
-					);
+					message.channel.send("<@" + user.id + ">, **" + message.guild.members.get(user.id).displayName + "** cleared all your warnings! You must have been lucky! :four_leaf_clover: ");
 				}
 			} else {
-				message
-					.reply("Bots don't have warnings, or do they :thinking:")
-					.then((msg) => setTimeout(() => msg.delete(), 10 * 1000));
+				message.reply("Bots don't have warnings, or do they :thinking:").then((msg) => setTimeout(() => msg.delete(), 10 * 1000));
 			}
 		}
 	}
@@ -36,7 +28,7 @@ exports.run = async (client, message, params) => {
 
 exports.conf = {
 	enabled: true,
-	aliases: [ 'cwarn' ],
+	aliases: [ 'cwarn', "cwarnings", "clearwarnings" ],
 	permLevel: 3
 };
 
