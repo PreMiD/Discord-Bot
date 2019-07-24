3a80f8453f7edadf38141477e048a2954359f9da
Timeraa <metz.florian@arcor.de>
2019-07-24 16:33:35 +0200

    🐛 Fix beta users not getting their roles & access

--- src/modules/betaAccess/events/guildMemberUpdate.ts
+++ src/modules/betaAccess/events/guildMemberUpdate.ts
@@ -1,11 +1,41 @@
 import * as Discord from "discord.js";
+import { MongoClient } from "../../../database/client";
 
-var { supporter, patron } = require("../../../roles.json");
+var { supporter, patron, beta, booster } = require("../../../roles.json"),
+  coll = MongoClient.db("PreMiD").collection("betaAccess");
 
 module.exports = async (
-  _oldMember: Discord.GuildMember,
+  oldMember: Discord.GuildMember,
   newMember: Discord.GuildMember
 ) => {
-  if (newMember.roles.has(patron) && !newMember.roles.has(supporter))
-    newMember.roles.add(supporter);
+  //* If user is patron and does not have either supporter or beta role, give it to them
+  if (
+    newMember.roles.has(patron) &&
+    (!newMember.roles.has(supporter) || !newMember.roles.has(beta))
+  ) {
+    newMember.roles.add([supporter, beta]);
+
+    if (!(await coll.findOne({ userId: newMember.id })))
+      await coll.insertOne({ userId: newMember.id });
+
+    return;
+  }
+
+  //* If user boosts and doesn't have beta role, give it to them
+  if (newMember.roles.has(booster) && !newMember.roles.has(beta)) {
+    newMember.roles.add(beta);
+
+    coll.insertOne({ userId: newMember.id });
+
+    return;
+  }
+
+  //* Remove beta access when boost expires
+  if (oldMember.roles.has(booster) && !newMember.roles.has(booster)) {
+    newMember.roles.remove(beta);
+
+    coll.findOneAndDelete({ userId: newMember.id });
+
+    return;
+  }
 };
--- src/modules/betaAccess/index.ts
+++ src/modules/betaAccess/index.ts
@@ -1,31 +1,17 @@
-import * as Discord from "discord.js";
-import { MongoClient } from "../../database/client";
 import { client } from "../..";
 
-var coll = MongoClient.db("PreMiD").collection("betaAccess"),
-  { developer, supporter, booster } = require("../../roles.json");
+var { supporter, booster, patron, beta } = require("../../roles.json");
 
 async function updateBetaAccess() {
-  var betaUsers = await coll.find().toArray(),
-    usersToAdd = (await client.guilds
-      .get("493130730549805057")
-      .members.fetch({ limit: 0 }))
-      .filter(
-        m =>
-          m.roles.has(developer) ||
-          m.roles.has(supporter) ||
-          m.roles.has(booster)
-      )
-      .map(m => m.id);
+  var betaUser = (await client.guilds
+    .get("493130730549805057")
+    .members.fetch({ limit: 0 })).filter(
+    m => m.roles.has(supporter) || m.roles.has(patron) || m.roles.has(booster)
+  );
 
-  var usersToAdd1 = usersToAdd
-    .filter(uTA => betaUsers.includes(uTA))
-    .map(m => {
-      return { userId: m };
-    });
-
-  if (usersToAdd1.length > 0) coll.insertMany(usersToAdd1);
+  betaUser.map(bU => {
+    if (!bU.roles.has(beta)) bU.roles.add(beta);
+  });
 }
 
 updateBetaAccess();
-setInterval(updateBetaAccess, 30 * 1000);
