98a932430d4f506fe7d86c38b87c80f9e786fdd3
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-04 15:41:05 +0300

    :sparkles: Updated role name.

--- src/modules/betaAccess/events/guildMemberUpdate.ts
+++ src/modules/betaAccess/events/guildMemberUpdate.ts
@@ -1,19 +1,19 @@
 import * as Discord from "discord.js";
 import { MongoClient } from "../../../database/client";
 
-var { supporter, patron, beta, booster } = require("../../../roles.json"),
+var { betaTester, patron, beta, booster } = require("../../../roles.json"),
   coll = MongoClient.db("PreMiD").collection("betaAccess");
 
 module.exports = async (
   oldMember: Discord.GuildMember,
   newMember: Discord.GuildMember
 ) => {
-  //* If user is patron and does not have either supporter or beta role, give it to them
+  //* If user is patron and does not have either betaTester or beta role, give it to them
   if (
     newMember.roles.has(patron) &&
-    (!newMember.roles.has(supporter) || !newMember.roles.has(beta))
+    (!newMember.roles.has(betaTester) || !newMember.roles.has(beta))
   ) {
-    newMember.roles.add([supporter, beta]);
+    newMember.roles.add([betaTester, beta]);
 
     if (!(await coll.findOne({ userId: newMember.id })))
       await coll.insertOne({ userId: newMember.id });
@@ -23,7 +23,7 @@ module.exports = async (
 
   //* If user boosts and doesn't have beta role, give it to them
   if (newMember.roles.has(booster) && !newMember.roles.has(beta)) {
-    newMember.roles.add([beta, supporter]);
+    newMember.roles.add([beta, betaTester]);
 
     coll.insertOne({ userId: newMember.id });
 
@@ -33,7 +33,7 @@ module.exports = async (
   //* Remove beta access when boost expires
   if (oldMember.roles.has(booster) && !newMember.roles.has(booster)) {
     newMember.roles.remove(beta);
-    if (!oldMember.roles.has(patron)) newMember.roles.remove(supporter);
+    if (!oldMember.roles.has(patron)) newMember.roles.remove(betaTester);
 
     coll.findOneAndDelete({ userId: newMember.id });
 
