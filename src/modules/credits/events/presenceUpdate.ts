c55340358f9a24019726735ffef2a9e2fa65fb69
Florian Metz <metz.florian@arcor.de>
2019-10-04 02:09:06 +0200

    ✨ Update credit user when they change status

---
+++ src/modules/credits/events/presenceUpdate.ts
@@ -0,0 +1,20 @@
+import * as Discord from "discord.js";
+import { MongoClient } from "../../../database/client";
+
+module.exports = async (
+  oldMember: Discord.GuildMember,
+  newMember: Discord.GuildMember
+) => {
+  var creditsCollection = MongoClient.db("PreMiD").collection("credits");
+  if (!newMember && !newMember.user) return;
+  if (!(await creditsCollection.findOne({ userId: newMember.user.id }))) return;
+
+  creditsCollection.updateOne(
+    { userId: newMember.user.id },
+    {
+      $set: {
+        status: newMember.user.presence.status
+      }
+    }
+  );
+};
