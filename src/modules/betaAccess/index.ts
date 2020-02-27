5e73162de1ff2cafeffe322ea9366d356dfeb9fd
Florian Metz <me@timeraa.dev>
2020-02-27 22:36:02 +0100

    🐛 Prevent 6.000+ MongoDB requests at startup

--- src/modules/betaAccess/index.ts
+++ src/modules/betaAccess/index.ts
@@ -4,10 +4,14 @@ import { info } from "../../util/debug";
 import roles from "../../roles";
 
 let coll = pmdDB.collection("betaAccess");
-let betaUsers = pmdDB.collection("betaUsers");
+let betaUserColl = pmdDB.collection("betaUsers");
 let discordUsers = pmdDB.collection("discordUsers");
 
 async function updateBetaAccess() {
+	const betaUsers = await coll
+		.find({}, { projection: { _id: false, userId: true } })
+		.toArray();
+
 	let betaUser = (
 		await client.guilds.cache
 			.get("493130730549805057")
@@ -18,28 +22,43 @@ async function updateBetaAccess() {
 
 	betaUser.map(async bU => {
 		if (!bU.roles.cache.has(roles.beta)) bU.roles.add(roles.beta);
-		if (!(await coll.findOne({ userId: bU.id })))
+		if (!betaUsers.find(u => u.userId === bU.id))
 			coll.insertOne({ userId: bU.id });
 	});
 }
 
 async function updateDiscordUsers() {
-	let guildMembers = await client.guilds.cache.get("493130730549805057").members.fetch({ limit: 0 });
+	const dbUsers = await discordUsers
+		.find({}, { projection: { _id: false, userId: true } })
+		.toArray();
+
+	let guildMembers = await client.guilds.cache
+		.get("493130730549805057")
+		.members.fetch({ limit: 0 });
 
 	guildMembers.map(async user => {
-		let dbUser = await discordUsers.findOne({ userId: user.id });
-		if (!dbUser) discordUsers.insertOne({ userId: user.id });
+		if (!dbUsers.find(u => u.userId === user.id))
+			discordUsers.insertOne({ userId: user.id });
 	});
 }
 
 async function updateBetaUsers() {
-	let guildMembers = await client.guilds.cache.get("493130730549805057").members.fetch({ limit: 0 });
+	const betaUsers = await betaUserColl
+		.find({}, { projection: { _id: false, userId: true } })
+		.toArray();
+
+	let guildMembers = await client.guilds.cache
+		.get("493130730549805057")
+		.members.fetch({ limit: 0 });
 
 	info("Updating beta users...");
 
 	guildMembers.map(async user => {
-		let betaUser = await betaUsers.findOne({ userId: user.id });
-		if (betaUser && !user.roles.cache.has(roles.beta)) user.roles.add(roles.beta);
+		if (
+			betaUsers.find(u => u.userId === user.id) &&
+			!user.roles.cache.has(roles.beta)
+		)
+			user.roles.add(roles.beta);
 	});
 
 	info("Updated beta users.");
