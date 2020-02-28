9a3ee9ea190ad0f226a0d65b73479601f22f6d8c
Florian Metz <me@timeraa.dev>
2020-02-28 19:08:54 +0100

    🚮 Remove unecessary protection

--- src/modules/moderation/events/message.ts
+++ src/modules/moderation/events/message.ts
@@ -1,16 +1,7 @@
 import * as Discord from "discord.js";
 import { client } from "../../..";
-import roles from "../../../roles";
-import messageFilter from "../messageFilter";
-import channels from "../../../channels";
 
 module.exports.run = async (message: Discord.Message) => {
-	let filterResult = messageFilter.find(m =>
-		message.content
-			.toLowerCase()
-			.match(new RegExp(m.message.toLowerCase(), "i"))
-	);
-
 	//* Ignore bots
 	if (message.author.bot) return;
 
@@ -20,43 +11,6 @@ module.exports.run = async (message: Discord.Message) => {
 			.then(msg => msg.delete({ timeout: 10 * 1000 }));
 		message.delete();
 	}
-
-	//* Return if permission level > 0
-	if (!filterResult) return;
-
-	if (filterResult.mute) {
-		message.member.roles.add(roles.muted);
-
-		let embed = new Discord.MessageEmbed({
-			author: {
-				name: message.member.displayName,
-				iconURL: message.author.displayAvatarURL({ size: 128 })
-			},
-			color: "#fc3c3c",
-			timestamp: new Date(),
-			fields: [
-				{
-					name: "Channel",
-					value: `<#${message.channel.id}>`
-				},
-				{
-					name: "Message",
-					value: message.content
-				}
-			]
-		});
-
-		if (message.guild.channels.cache.has(channels.moderators))
-			(message.guild.channels.cache.get(
-				channels.moderators
-			) as Discord.TextChannel).send("Please check this mute!", {
-				embed: embed
-			});
-	}
-
-	message
-		.reply(filterResult.botMessage)
-		.then((msg: Discord.Message) => msg.delete({ timeout: 15 * 1000 }));
 };
 
 module.exports.config = {};
