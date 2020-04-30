12ac23b4825b0e5aeab0a0d624b40679f92cac28
Florian Metz <me@timeraa.dev>
2020-04-30 21:11:35 +0200

    🎨 Always show logs, because yes

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -1,17 +1,13 @@
-let chalk = undefined;
-if (process.env.NODE_ENV == "dev") chalk = require("chalk");
+import chalk from "chalk";
 
 export function info(message: string) {
-  if (process.env.NODE_ENV != "dev") return;
-  console.log(`${chalk.bgBlue(chalk.white(" INFO "))} ${message}`);
+	console.log(`${chalk.bgBlue(chalk.white(" INFO "))} ${message}`);
 }
 
 export function success(message: string) {
-  if (process.env.NODE_ENV != "dev") return;
-  console.log(`${chalk.bgGreen(" SUCCESS ")} ${message}`);
+	console.log(`${chalk.bgGreen(" SUCCESS ")} ${message}`);
 }
 
 export function error(message: string) {
-  if (process.env.NODE_ENV != "dev") return;
-  console.log(`${chalk.bgRed(" ERROR ")} ${message}`);
+	console.log(`${chalk.bgRed(" ERROR ")} ${message}`);
 }
