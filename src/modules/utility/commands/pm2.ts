8d5b1dc355798d4ae4adc1774e1dc4bd5d19f5a2
Florian Metz <me@timeraa.dev>
2020-05-07 23:34:16 +0200

    🛠 No idea if this works but let's see

---
+++ src/modules/utility/commands/pm2.ts
@@ -0,0 +1,67 @@
+import * as Discord from "discord.js";
+import pm2 from "pm2";
+
+module.exports.run = async (
+	message: Discord.Message,
+	params: Array<string>
+) => {
+	message.delete();
+
+	pm2.connect(true, async function (err) {
+		if (err) {
+			message.reply(`Error connecting to pm2: \`\`\`${err.message}\`\`\``);
+			return;
+		}
+		pm2.list((err, processes) => {
+			if (err) {
+				message.reply(
+					`Error listing pm2 processes: \`\`\`${err.message}\`\`\``
+				);
+				return;
+			}
+
+			processes = processes.filter(p =>
+				p.pm2_env.pm_exec_path.includes("PreMiD")
+			);
+			if (params.length === 0) {
+				message.channel.send(
+					new Discord.MessageEmbed({
+						title: "pm2 - Processes",
+						fields: processes.map(p => {
+							return {
+								name: `${p.name} - ${p.pm2_env.status}`,
+								value: `${p.monit.cpu}% | ${Math.floor(
+									p.monit.memory / 1024 / 1024
+								)}mb`
+							};
+						})
+					})
+				);
+				pm2.disconnect();
+			} else {
+				const processToRestart = processes.find(
+					p => p.name === params.join(" ")
+				);
+				if (processToRestart) {
+					pm2.reload(params.join(" "), (err, p) => {
+						if (err) {
+							message.reply(
+								`Error reloading process: \`\`\`${err.message}\`\`\``
+							);
+							return;
+						}
+
+						message.reply("Successfully reloaded process.");
+						pm2.disconnect();
+					});
+				}
+			}
+		});
+	});
+};
+
+module.exports.config = {
+	name: "pm2",
+	description: "Execute pm2 commands.",
+	permLevel: 4
+};
