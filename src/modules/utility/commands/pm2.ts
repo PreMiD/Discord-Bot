dbb6ae672ca004d1c2906c505add5576d3825a3a
Florian Metz <me@timeraa.dev>
2020-05-08 00:34:30 +0200

    ♻ More refinements

--- src/modules/utility/commands/pm2.ts
+++ src/modules/utility/commands/pm2.ts
@@ -12,7 +12,7 @@ module.exports.run = async (
 			message.reply(`Error connecting to pm2: \`\`\`${err.message}\`\`\``);
 			return;
 		}
-		pm2.list((err, processes) => {
+		pm2.list(async (err, processes) => {
 			if (err) {
 				message.reply(
 					`Error listing pm2 processes: \`\`\`${err.message}\`\`\``
@@ -22,27 +22,36 @@ module.exports.run = async (
 
 			processes = processes.filter(p => p.pm2_env.pm_cwd.includes("PreMiD"));
 			if (params.length === 0) {
-				message.channel.send(
-					new Discord.MessageEmbed({
-						title: "pm2 - Processes",
-						color: "#7289DA",
-						fields: processes.map(p => {
-							return {
-								name: `${p.name} - ${p.pm2_env.status}`,
-								value: `**CPU:** \`${p.monit.cpu}%\` | **RAM:** \`${Math.floor(
-									p.monit.memory / 1024 / 1024
-								)}mb\``
-							};
+				(
+					await message.channel.send(
+						new Discord.MessageEmbed({
+							title: "pm2 - Processes",
+							color: "#7289DA",
+							fields: processes
+								.filter((p, i) => processes.indexOf(p) === i)
+								.map(p => {
+									return {
+										name: `${p.name} - ${
+											p.pm2_env.status.slice(0, 1).toUpperCase() +
+											p.pm2_env.status.slice(1, p.pm2_env.status.length)
+										}`,
+										value: `**CPU:** \`${
+											p.monit.cpu
+										}%\` | **RAM:** \`${Math.floor(
+											p.monit.memory / 1024 / 1024
+										)}mb\``
+									};
+								})
 						})
-					})
-				);
+					)
+				).delete({ timeout: 15 * 1000 });
 				pm2.disconnect();
 			} else {
 				const processToRestart = processes.find(
 					p => p.name === params.join(" ")
 				);
 				if (processToRestart) {
-					pm2.reload(params.join(" "), (err, p) => {
+					pm2.reload(params.join(" "), async (err, p) => {
 						if (err) {
 							message.reply(
 								`Error reloading process: \`\`\`${err.message}\`\`\``
@@ -50,7 +59,9 @@ module.exports.run = async (
 							return;
 						}
 
-						message.reply("Successfully reloaded process.");
+						(await message.reply("Successfully reloaded process.")).delete({
+							timeout: 5 * 1000
+						});
 						pm2.disconnect();
 					});
 				}
