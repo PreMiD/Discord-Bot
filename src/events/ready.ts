6815ff375bd4af351f5b8a77bef685469b6df87f
John Burke <johnburke934@gmail.com>
2020-05-31 02:10:18 -0700

    Auto presence role

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,25 @@
 import { success } from "../util/debug";
-
+import {pmdDB} from "../database/client";
 import * as Discord from "discord.js";
+import roles from "../roles";
+const col = pmdDB.collection("presences");
 
-module.exports.run = (client: Discord.Client) => {
-  if (client.user) success(`Connected as ${client.user.tag}`);
+module.exports.run = async (client: Discord.Client) => {
+	if (client.user) success(`Connected as ${client.user.tag}`);
+	let oldPresences = (await col.find().toArray()).map(x => x._id.toString());
+	setInterval(async () => {
+		const newPresences = (await col.find().toArray()).map(x => x._id.toString());
+		const actual = newPresences.filter(x => !oldPresences.includes(x));
+		for (const presence of actual) {
+			const member = client.guilds.cache.get("493130730549805057").members.cache.get(presence.metadata.author.id);
+			if (!member) continue;
+			if (!member.roles.cache.has(roles.presence)) {
+				await member.roles.add(roles.presence);
+			}
+		}
+	}, 60000 * 15);
 };
 
 module.exports.config = {
-  clientOnly: true
+	clientOnly: true
 };
