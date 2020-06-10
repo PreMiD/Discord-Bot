210b2aa4a64fc4beff0f77d02ef8b6db97d0eaed
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-06-10 04:12:33 +0100

    Update messageFiltered.ts

--- src/messageFiltered.ts
+++ src/messageFiltered.ts
@@ -1,12 +1,12 @@
 export default [
 	{
-		message: "privatepage.vip|nakedphotos.club",
+		message: "privatepage.vip|nakedphotos.club|pornhub.com",
 		botMessage:
 			"**You have been muted, please wait while our moderators check it**",
 		mute: true
 	},
 	{
-		message: "discordapp.com/invite/|discord.gg/",
+		message: "discordapp.com/invite/|discord.com/invite/|discord.gg/",
 		botMessage: "**Invite links are not allowed on this server!**",
 		mute: false
 	}
