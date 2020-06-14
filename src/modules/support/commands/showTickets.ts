61298fdc9bc074b2358948ead4b47fcd6ee6d4df
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-06-14 11:45:40 +0100

    Update showTickets.ts

--- src/modules/support/commands/showTickets.ts
+++ src/modules/support/commands/showTickets.ts
@@ -7,19 +7,20 @@ module.exports.run = async (
 ) => {
 		const tickets = await tcoll.find({ status: 1 }).toArray();
 
-		tickets.map(async t => {
-			const ticket = new Ticket(),
-				ticketFound = await ticket.fetch("channel", t.supportChannel);
+	tickets.map(async t => {
+		const ticket = new Ticket(), ticketFound = await ticket.fetch("channel", t.supportChannel);
 
-			if (!ticketFound) return;
-			ticket.channel.updateOverwrite(message.member, {
-				VIEW_CHANNEL: true,
-				SEND_MESSAGES: true,
-				EMBED_LINKS: true,
-				ATTACH_FILES: true,
-				USE_EXTERNAL_EMOJIS: true
-			});
-		});
+		if (!ticketFound) return;
+		ticket.channel.updateOverwrite(message.member, {
+			VIEW_CHANNEL: true,
+			SEND_MESSAGES: true,
+			EMBED_LINKS: true,
+			ATTACH_FILES: true,
+			USE_EXTERNAL_EMOJIS: true
+		});	
+		
+		message.reply("you can now see all tickets!").then(msg => msg.delete({ timeout: 10 * 1000 }));
+	});
 };
 
 module.exports.config = {
