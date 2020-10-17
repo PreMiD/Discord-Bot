d120afaaf41fc547c9581bf38061d8c2cddf1452
Callum OKane <=>
2020-10-17 21:08:07 +0100

    👋 Bye!

--- src/events/guildBanAdd.ts
+++
@@ -1,8 +0,0 @@
-import * as Discord from "discord.js";
-import { pmdDB } from "../database/client";
-
-const coll = pmdDB.collection("userSettings");
-
-module.exports = (_: Discord.Guild, user: Discord.User) => {
-	coll.findOneAndDelete({ userId: user.id });
-};
