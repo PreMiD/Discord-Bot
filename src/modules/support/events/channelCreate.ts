4612dd35f24ca6bacffdd072e6039013bc327023
ririxi <mail@ririxi.dev>
2020-12-28 16:17:22 +0100

    🔥 Change finding users

--- src/modules/support/events/channelCreate.ts
+++ src/modules/support/events/channelCreate.ts
@@ -8,21 +8,19 @@ const coll = pmdDB.collection("userSettings");
 
 module.exports = async (channel: GuildChannel) => {
 	if (channel.parentID === channels.ticketCategory) {
-		const users = await coll.find({}).toArray();
+		const users = await coll.find({ seeAllTickets: true }).toArray();
 		users.map(async user => {
-			if (user.seeAllTickets) {
-				const ticket = new Ticket(),
-					ticketFound = await ticket.fetch("channel", channel.id);
+			const ticket = new Ticket(),
+				ticketFound = await ticket.fetch("channel", channel.id);
 
-				if (!ticketFound) return;
-				ticket.channel?.updateOverwrite(user.userId, {
-					VIEW_CHANNEL: true,
-					SEND_MESSAGES: true,
-					EMBED_LINKS: true,
-					ATTACH_FILES: true,
-					USE_EXTERNAL_EMOJIS: true
-				});
-			}
+			if (!ticketFound) return;
+			ticket.channel?.updateOverwrite(user.userId, {
+				VIEW_CHANNEL: true,
+				SEND_MESSAGES: true,
+				EMBED_LINKS: true,
+				ATTACH_FILES: true,
+				USE_EXTERNAL_EMOJIS: true
+			});
 		});
 	}
 };
