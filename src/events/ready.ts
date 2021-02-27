7a0e945bb3b7193209c7e20ca3b855e07ea1812d
callumok2004 <callumokane123@gmail.com>
2021-02-27 14:19:18 +0000

    Deepscan be going deep

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,15 +1,12 @@
-let roles, client;
-
-const col = client.db.collection("presences");
-
 module.exports = {
     name: "ready",
     type: "client",
-    run: (client1) => {
-        roles = client.config.roles;
-        client = client1;
+    run: (client) => {
+        const roles = client.config.roles,
+            col = client.db.collection("presences");
 
-        client.success(`Connected as ${client1.user.tag}`);
+
+        client.success(`Connected as ${client.user.tag}`);
         client.user.setActivity(`premid.app`);
 
         require("../modules/status/");
@@ -20,52 +17,52 @@ module.exports = {
             updatePresenceAuthors();
             updateBoosters();
         }, 15 * 60 * 1000)
+
+        async function updatePresenceAuthors() {
+            const guild = client.guilds.cache.get("493130730549805057"),
+                presences = await col.find().toArray(),
+                presenceDevelopers = presences.map(p => p.metadata.author.id);
+        
+            presenceDevelopers.concat(
+                presences
+                    .filter(p => p.metadata.contributors)
+                    .map(p => p.metadata.contributors.map(x => x.id))
+                    .join()
+                    .split(",")
+            );
+
+            for (const author of presenceDevelopers) {
+                const member = guild.members.resolve(author);
+                if (member && !member.roles.cache.has(roles.presence))
+                    member.roles.add(roles.presence);
+            }
+        }
+
+        function updateBoosters() {
+            const dateNow = new Date(),
+                last90days = new Date(
+                    dateNow.setDate(dateNow.getDate() - 3 * 30)
+                ).getTime(),
+                membersWithBoost = client.guilds.cache
+                    .get("493130730549805057")
+                    .members.cache.array()
+                    .filter(member => member.premiumSinceTimestamp > 0);
+
+            for (const member of membersWithBoost) {
+                if (member) {
+                    const userBoost = member.premiumSinceTimestamp;
+
+                    if (last90days > userBoost) {
+                        if (!member.roles.cache.has(roles.donator))
+                            member.roles.add(roles.donator);
+                        if (!member.roles.cache.has(roles.alpha)) {
+                            if (member.roles.cache.has(roles.beta))
+                                member.roles.remove(roles.beta);
+                            member.roles.add(roles.alpha);
+                        }
+                    }
+                }
+            }
+        }
     }
-}
-
-async function updatePresenceAuthors() {
-	const guild = client.guilds.cache.get("493130730549805057"),
-		presences = await col.find().toArray(),
-		presenceDevelopers = presences.map(p => p.metadata.author.id);
-
-	presenceDevelopers.concat(
-		presences
-			.filter(p => p.metadata.contributors)
-			.map(p => p.metadata.contributors.map(x => x.id))
-			.join()
-			.split(",")
-	);
-
-	for (const author of presenceDevelopers) {
-		const member = guild.members.resolve(author);
-		if (member && !member.roles.cache.has(roles.presence))
-			member.roles.add(roles.presence);
-	}
-}
-
-function updateBoosters() {
-	const dateNow = new Date(),
-		last90days = new Date(
-			dateNow.setDate(dateNow.getDate() - 3 * 30)
-		).getTime(),
-		membersWithBoost = client.guilds.cache
-			.get("493130730549805057")
-			.members.cache.array()
-			.filter(member => member.premiumSinceTimestamp > 0);
-
-	for (const member of membersWithBoost) {
-		if (member) {
-			const userBoost = member.premiumSinceTimestamp;
-
-			if (last90days > userBoost) {
-				if (!member.roles.cache.has(roles.donator))
-					member.roles.add(roles.donator);
-				if (!member.roles.cache.has(roles.alpha)) {
-					if (member.roles.cache.has(roles.beta))
-						member.roles.remove(roles.beta);
-					member.roles.add(roles.alpha);
-				}
-			}
-		}
-	}
 }
