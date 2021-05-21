be470df3c7d200ec4a0a02756d62b922f5a2876e
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2021-05-21 17:54:14 +0100

    Fix Role Color

--- src/database/client.ts
+++ src/database/client.ts
@@ -6,16 +6,11 @@ export let pmdDB: Db;
 export function connect() {
 	return new Promise<mongoClient>((resolve, reject) => {
 		mongoClient
-			.connect(
-				`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${
-					process.env.MONGOIP
-				}:${27017}`,
-				{
-					useUnifiedTopology: true,
-					useNewUrlParser: true,
-					appname: "PreMiD Discord-Bot"
-				}
-			)
+			.connect(process.env.MONGO_URL, {
+				useUnifiedTopology: true,
+				useNewUrlParser: true,
+				appname: "PreMiD Discord-Bot"
+			})
 			.then(mongoClient => {
 				MongoClient = mongoClient;
 				pmdDB = MongoClient.db("PreMiD");
