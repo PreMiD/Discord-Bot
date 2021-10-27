3086c342774c5b19ee62f345bebda0a5701992ca
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2021-10-27 14:35:46 +0100

    Update client.ts

--- src/database/client.ts
+++ src/database/client.ts
@@ -11,7 +11,7 @@ export function connect() {
 			})
 			.then(mongoClient => {
 				MongoClient = mongoClient;
-				pmdDB = MongoClient.db("PreMiD-DEV");
+				pmdDB = MongoClient.db("PreMiD");
 				resolve(mongoClient);
 			})
 			.catch(reject);
