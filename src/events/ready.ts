f2cccc246f6557f136c1366505082080f8d4f760
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-12-26 19:28:42 +0000

    feat: randomly select a presence for the status
    feat: randomly select a presence for the status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,48 +3,66 @@ import { Presences } from "../../@types/interfaces";
 import config from "../config";
 
 export default async function () {
-	await managePresenceDevelopers();
-	setInterval(managePresenceDevelopers, 15 * 60 * 1000);
+  await managePresenceDevelopers();
+  setInterval(managePresenceDevelopers, 15 * 60 * 1000);
+
+  updateStatusActivity();
+  setInterval(updateStatusActivity, 2 * 60 * 1000);
+}
+
+async function updateStatusActivity(): Promise<void> {
+  const presences = (
+      await pmdDB.collection<Presences>("presences").find({}).toArray()
+    ).map(presence => presence.metadata),
+    randomPresence = presences[Math.floor(Math.random() * presences.length)];
+  client.user?.setActivity(randomPresence.service, {
+    type:
+      randomPresence.category === "music"
+        ? "LISTENING"
+        : randomPresence.category === "videos"
+        ? "WATCHING"
+        : "PLAYING"
+  });
 }
 
 async function managePresenceDevelopers() {
-	const log = mainLog.extend("managePresenceDevelopers");
+  const log = mainLog.extend("managePresenceDevelopers");
 
-	log("Updating...");
-	const presenceDevs = [
-		...new Set(
-			(
-				await pmdDB
-					.collection<Presences>("presences")
-					.find(
-						{},
-						{
-							projection: {
-								"metadata.author.id": true,
-								"metadata.contributors.id": true,
-								_id: false
-							}
-						}
-					)
-					.map(u => [
-						u.metadata.author.id,
-						...(u.metadata.contributors?.map(c => c.id) || [])
-					])
-					.toArray()
-			).flat()
-		)
-	];
+  log("Updating...");
+  const presenceDevs = [
+    ...new Set(
+      (
+        await pmdDB
+          .collection<Presences>("presences")
+          .find(
+            {},
+            {
+              projection: {
+                "metadata.author.id": true,
+                "metadata.contributors.id": true,
+                _id: false
+              }
+            }
+          )
+          .map(u => [
+            u.metadata.author.id,
+            ...(u.metadata.contributors?.map(c => c.id) || [])
+          ])
+          .toArray()
+      ).flat()
+    )
+  ];
 
-	const pmdGuild = await client.guilds.fetch(config.guildId);
+  const pmdGuild = await client.guilds.fetch(config.guildId);
 
-	for (const pDev of presenceDevs)
-		try {
-			const member = await pmdGuild.members.fetch(pDev);
-			if (!member.roles.cache.has(config.roles.presenceDev)) {
-				await member.roles.add(config.roles.presenceDev);
-				log("Added missing role to %s", member.user.tag);
-			}
-		} catch (err) {}
+  for (const pDev of presenceDevs)
+    try {
+      const member = await pmdGuild.members.fetch(pDev);
+      if (!member.roles.cache.has(config.roles.presenceDev)) {
+        await member.roles.add(config.roles.presenceDev);
+        log("Added missing role to %s", member.user.tag);
+      }
+    } catch (err) {}
 
-	log("Updated!");
+  log("Updated!");
 }
