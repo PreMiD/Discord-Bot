c31eee0f422b903dd202c03ac93bc512eb2324f8
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2022-02-05 23:30:05 +0000

    g

--- src/modules/utility/credits/events/guildMemberRemove.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_module_loader_1 = require("discord-module-loader");
-const __1 = require("../../..");
-exports.default = new discord_module_loader_1.DiscordEvent("guildMemberRemove", async (member) => {
-    await __1.pmdDB.collection("credits").deleteOne({ userId: member.id });
-});
-//# sourceMappingURL=guildMemberRemove.js.map
--- src/modules/utility/credits/events/guildMemberRemove.ts
+++
@@ -1,9 +0,0 @@
-import { DiscordEvent } from "discord-module-loader";
-import { GuildMember } from "discord.js";
-
-import { pmdDB } from "../../..";
-import { Credits } from "../../../../@types/interfaces";
-
-export default new DiscordEvent("guildMemberRemove", async member => {
-	await pmdDB.collection<Credits>("credits").deleteOne({ userId: member.id });
-});
