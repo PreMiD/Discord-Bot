652483d1062c78bc732bb06d9c1bc1e1198ca31d
hayper <hayper1919@gmail.com>
2024-05-27 01:18:21 +0000

    feat: ground work

--- src/events/guildMemberRemove.ts
+++
@@ -1,8 +0,0 @@
-import { DiscordEvent } from "discord-module-loader";
-
-import { pmdDB } from "..";
-import { DiscordUsers } from "../../@types/interfaces";
-
-export default new DiscordEvent("guildMemberRemove", async member => {
-	await pmdDB.collection<DiscordUsers>("discordUsers").deleteOne({ userId: member.id });
-});
--- src/events/guildMemberUpdate.ts
+++
@@ -1,7 +0,0 @@
-import { DiscordEvent } from "discord-module-loader";
-
-import updateDiscordUser from "../functions/updateDiscordUser";
-
-export default new DiscordEvent("guildMemberUpdate", async (_, newMember) => {
-	await updateDiscordUser(newMember);
-});
